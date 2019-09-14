8a1230c8b192795c01f2c0487b046ef22ca08386
Pablo Bahillo <bahillo.ortego.pablo@gmail.com>
2019-09-14 21:27:41 +0200

    done Lesson 106

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -47,9 +47,26 @@ class _NewTransactionState extends State<NewTransaction> {
               controller: amountControler,
               onSubmitted: (_) => submitData(),
             ),
-            FlatButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Text('No Date Chosen!'),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            RaisedButton(
               child: Text('Add Transaction'),
-              textColor: Theme.of(context).primaryColor,
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
               onPressed: () {
                 print(titleControler.text);
                 submitData();
